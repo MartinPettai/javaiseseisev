da3c65402267538c436af13fa62785360e3ab012
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-09-26 17:55:03 +0300

    Add files via upload

---
+++ iseseisev_14.java
@@ -0,0 +1,19 @@
+public class iseseisev_14{
+  public static void main(String[] args){
+ 
+  int t4ht1='A'+(int)(Math.random()*('Y' - 'A'));
+  int t4ht2='A'+(int)(Math.random()*('Y' - 'A'));
+  int t4ht3='A'+(int)(Math.random()*('Y' - 'A')); 
+  
+  int nr1=(int)(Math.random()*10);
+  int nr2=(int)(Math.random()*10);
+  int nr3=(int)(Math.random()*10);
+
+  
+  System.out.println(""+nr1+nr2+nr3+" "+(char)(t4ht1)+(char)(t4ht2)+(char)(t4ht3));
+
+
+
+
+}
+}
