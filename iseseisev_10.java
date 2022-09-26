a80fab28ec2717e24adbbd144246e5be71beba22
MartinPettai <73310313+MartinPettai@users.noreply.github.com>
2022-09-26 17:38:42 +0300

    Add files via upload

---
+++ iseseisev_10.java
@@ -0,0 +1,16 @@
+public class iseseisev_10
+{
+public static void main(String args[])
+{
+int number=1;
+System.out.println("numbrid yhest 66'ni");
+
+while( number <=66 ){
+for ( int i=1;i<=5;i++ )
+{
+System.out.print(number);
+number++;}
+System.out.println(" ");
+}
+}
+}
